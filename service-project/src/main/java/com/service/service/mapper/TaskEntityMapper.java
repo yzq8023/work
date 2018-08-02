ab166448f3888816e8153c706f6d570fc9732a02
547401007@qq.com <yzq8023>
2018-08-02 14:05:18 +0800

    修改taskmapper等

--- service-project/src/main/java/com/service/service/mapper/TaskEntityMapper.java
+++ service-project/src/main/java/com/service/service/mapper/TaskEntityMapper.java
@@ -5,10 +5,11 @@ import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 public interface TaskEntityMapper extends Mapper<TaskEntity> {
     public void deleteTaskTeamsById(@Param("taskId") Integer taskId);
     public void insertTaskTeamsById(@Param("taskId") Integer taskId, @Param("teamId") Integer teamId);
-    public List<TaskEntity> selectTaskByPIdAndUId(@Param("userId") Integer userId, @Param("projectId") Integer projectId);
+    public List<Map<String,Object>> selectTaskByPIdAndUId(@Param("userId") Integer userId, @Param("projectId") Integer projectId);
     public List<TaskEntity> selectJoinedTaskById(@Param("userId") Integer userId);
 }
