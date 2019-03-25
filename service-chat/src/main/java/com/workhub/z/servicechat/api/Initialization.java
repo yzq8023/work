8e746b82bfcb41bb7265dd0d0f5038e2deb54862
TO-ZH <386845154@qq.com>
2019-03-25 16:05:05 +0800

    增加消息、讨论组mapper、model

---
+++ service-chat/src/main/java/com/workhub/z/servicechat/api/Initialization.java
@@ -0,0 +1,50 @@
+package com.workhub.z.servicechat.api;
+
+import com.github.hollykunge.security.api.vo.user.UserInfo;
+import com.workhub.z.servicechat.feign.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+*@Description: 系统用户初始化类
+*@Author: 忠
+*@date: 2019/3/21
+*/
+@Controller
+@RequestMapping("/api/Initialization")
+public class Initialization {
+
+//    @Autowired
+//    private IUserService iUserService;
+
+    /**
+    *@Description: 查询user所在群组
+    *@Param: userid
+    *@return: user群组列表
+    *@Author: 忠
+    *@date: 2019/3/21
+    */
+
+    /**
+    *@Description: 查询用户详细信息
+    *@Param: userid
+    *@return: usermodel
+    *@Author: 忠
+    *@date: 2019/3/21
+    */
+//    @RequestMapping("/getInfo")
+//    public UserInfo queryUserInfoBySn(String sn){
+//        iUserService.queryUserinfoBySn(sn);
+//        return null;
+//    }
+
+    /**
+    *@Description: 最近联系人
+    *@Param:userid
+    *@return:userlist
+    *@Author: 忠
+    *@date: 2019/3/21
+    */
+
+}
